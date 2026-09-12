105cd43b79fb4047a9ecdc3edc8ee614f59fb5e3
danilaplee <fiend.of.sound@gmail.com>
2026-09-12 20:29:35 +0200

    fix/lint

--- src/renders/renders.module.ts
+++ src/renders/renders.module.ts
@@ -7,13 +7,13 @@ import { config } from '../config';
 import { RenderProcessor } from './renders.processor';
 import { AuthService } from '../auth/auth.service';
 import { ApiKey } from '../model/apikey.entity';
-import { ResultProcessor } from './result.processor';
+// import { ResultProcessor } from './result.processor';
 import { LogsProcessor } from './logs.processor';
 import { LogPiece } from '../model/logpiece.entity';
 
 const optionalProviders = [
   ...(config.queue.isLogsRunner ? [LogsProcessor] : []),
-  ...(config.queue.isFileRunner ? [ResultProcessor] : []),
+  // ...(config.queue.isFileRunner ? [ResultProcessor] : []),
   ...(config.queue.isRenderRunner ? [RenderProcessor] : []),
 ];
 @Module({
