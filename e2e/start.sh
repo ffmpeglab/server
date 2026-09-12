6f3bd8266118bb3e0f44560f9862a6f0e51746e0
danilaplee <fiend.of.sound@gmail.com>
2026-09-12 20:42:21 +0200

    fix/lint

--- e2e/start.sh
+++ e2e/start.sh
@@ -159,9 +159,9 @@ echo -e "${GREEN} Preparing ffmpeg...${NC}"
 sudo apt-get install -y ffmpeg
 export FFMPEG_PATH=$(which ffmpeg)
 echo -e "${GREEN} Path to ffmpeg: ${FFMPEG_PATH}"
-sudo apt-get install bwrap
-export BWRAP_PATH=$(which bwrap)
-echo -e "${GREEN} bwrap installed"
+sudo apt-get install -y bubblewrap
+export BWRAP_PATH=$(which bubblewrap)
+echo -e "${GREEN} bwrap installed ${BWRAP_PATH}"
 
 echo -e "${BLUE}💾 Installing python s3 sdk${NC}"
 pip install boto3
